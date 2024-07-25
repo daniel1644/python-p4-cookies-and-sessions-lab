cbc283f0b7a9d510ef26e5c1079e73562fc0f5ae
daniel beyene <dazimore@gmail.com>
2024-07-25 15:18:20 +0300

    Cookies and Sessions Lab (CodeGrade)

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3 
 
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
@@ -20,15 +20,25 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
+
 @app.route('/articles')
 def index_articles():
+    articles = [article.to_dict() for article in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
-    pass
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        return Article.query.filter(Article.id == id).first().to_dict(), 200
+        
+    return {'message': 'Maximum pageview limit reached'}, 401
+    
+
 
 if __name__ == '__main__':
     app.run(port=5555)
